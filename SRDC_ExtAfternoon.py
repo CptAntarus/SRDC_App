ddb0cd88f63e6722701b97416b9fa9ef8fa4db56
CptAntarus <Dylanrhendrix@yahoo.com>
2025-07-25 18:03:38 -0400

    Removed extended care files

--- SRDC_ExtAfternoon.py
+++
@@ -1,6 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-
-class ExtAfternoon(Screen):
-    def on_enter(self):
-        print("Made it to ExtAfternoon")
--- SRDC_ExtMorning.py
+++
@@ -1,6 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-
-class ExtMorning(Screen):
-    def on_enter(self):
-        print("Made it to ExtMorning")
